e2511d07c38396cc724aaa337c18f658db37e195
BluePanda001 <veroniquecarlareagon@gmail.com>
2019-12-20 23:10:36 +0200

    Create SignUp.js

---
+++ src/components/auth/SignUp.js
@@ -0,0 +1,54 @@
+// creating a component for the sign up information
+
+import React, { Component } from 'react'
+
+class SignUp extends Component {
+    state={
+        email:'',
+        password:'',
+        firstName:'',
+        lastName:''
+
+    }
+    //method that updates the state variable to the value entered by the user
+    handleChange=(e)=>{
+        this.setState({
+            [e.target.id]:e.target.value
+        })
+    }
+    handleSubmit=(e)=>{
+        e.preventDefault();
+        console.log(this.state);
+    }
+    render() {
+        return (
+            <div className="container">
+                <form onSubmit={this.handleSubmit} className="white">
+                    <h5 className="grey-text text-darken-3">Sign Up</h5>
+                    <div className="input-field">
+                        <label htmlFor="email">Email</label>
+                        <input type="email" id="email" onChange={this.handleChange}/>
+                    </div>
+                    <div className="input-field">
+                        <label htmlFor="password">password</label>
+                        <input type="password" id="password" onChange={this.handleChange}/>
+                    </div>
+                    <div className="input-field">
+                        <label htmlFor="firstName">First Name</label>
+                        <input type="text" id="firstName" onChange={this.handleChange}/>
+                    </div>
+                    <div className="input-field">
+                        <label htmlFor="lastName">Last Name</label>
+                        <input type="text" id="lastName" onChange={this.handleChange}/>
+                    </div>
+                    <div className="input-field">
+                        <button className="btn blue lighten-4 z-depth-0 black-text"><strong>Sign Up</strong></button>
+                    </div>
+                </form>
+                
+            </div>
+        )
+    }
+}
+
+export default SignUp
