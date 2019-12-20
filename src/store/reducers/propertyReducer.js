b140baae5811cc8239fa771608358929f5e3a02d
BluePanda001 <veroniquecarlareagon@gmail.com>
2019-12-20 23:11:21 +0200

    Create propertyReducer.js

---
+++ src/store/reducers/propertyReducer.js
@@ -0,0 +1,12 @@
+const initState={
+    properties:[
+        {id:'1', title:'Constantia',content:'3 bedroom house'},
+        {id:'2', title:'Claremont',content:'4 bedroom house'},
+        {id:'3', title:'Tokai',content:'3 bedroom house'}
+    ]
+}
+
+const propertyReducer =(state=initState,action)=>{
+    return state
+}
+export default propertyReducer
