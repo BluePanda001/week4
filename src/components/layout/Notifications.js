7645813acf80e8ed58865ade3175ff94344c3e90
BluePanda001 <veroniquecarlareagon@gmail.com>
2019-12-20 23:10:44 +0200

    Create Notifications.js

---
+++ src/components/layout/Notifications.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Notifications =()=>{
+    return(         
+        <div className="col s15 m8">
+            <div className="container section project-details">
+                <div className="card z-depth-0">
+                    <div className="card-content">
+                        <h5 className="red-text ">New Property Added By Joe Soap</h5>
+                        <p>Located in Houtbay, the asking price is R 12 million</p>
+                    </div>                            
+                </div>
+            </div>                       
+        </div>  
+    )
+}
+export default Notifications
