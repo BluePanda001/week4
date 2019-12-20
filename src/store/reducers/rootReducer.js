6540f234661b3013c45af7c5b11d5c8775858baf
BluePanda001 <veroniquecarlareagon@gmail.com>
2019-12-20 23:11:24 +0200

    Create rootReducer.js

---
+++ src/store/reducers/rootReducer.js
@@ -0,0 +1,11 @@
+//combining the auth and property reducers into one reducer
+import authReducer from './authReducer'
+import propertyReducer from './propertyReducer'
+import {combineReducers} from 'redux'
+
+const rootReducer= combineReducers({
+    auth:authReducer,
+    property:propertyReducer
+});
+
+export default rootReducer
